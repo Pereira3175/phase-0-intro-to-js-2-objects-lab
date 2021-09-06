bca9231f6800888a08a734959ba3af8feeaa7681
Luis <pereira1214@gmail.com>
2021-09-06 11:09:45 -0400

    commit tree 43485d4f302f1ea1062ab5ac90a2f5a6921d9c73

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ function destructivelyDeleteFromEmployeeByKey() {
     return employee
 
 }
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
